2171b282c2102f922dd82f32f003758d7e010d8e
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-11-05 17:26:44 -0200

    Criado componente loading para verificar a cobertura do codacy tslint

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -13,12 +13,14 @@ import { ExtensaoModule } from './extensao/extensao.module';
 import { GestaoModule } from './gestao/gestao.module';
 import { PesquisaModule } from './pesquisa/pesquisa.module';
 import { AppRoutingModule } from './app-routing.module';
+import { LoadingComponent } from './shared/loading/loading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
