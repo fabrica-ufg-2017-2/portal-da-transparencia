3c3479b4faf901570227dc8a28105b6702b40470
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-11-05 20:07:07 -0200

    Estabelecendo arquitetura de pacotes para o módulo 'Extensão'

--- front/src/app/extensao/extensao-routing.module.ts
+++ front/src/app/extensao/extensao-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ExtensaoComponent } from './extensao/extensao.component';
+import { ExtensaoComponent } from './screens/extensao/extensao.component';
 
 const routes: Routes = [
   {
--- front/src/app/extensao/screens/extensao/extensao.component.spec.ts
+++ front/src/app/extensao/screens/extensao/extensao.component.spec.ts
@@ -6,12 +6,13 @@ describe('ExtensaoComponent', () => {
   let component: ExtensaoComponent;
   let fixture: ComponentFixture<ExtensaoComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ExtensaoComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ExtensaoComponent]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ExtensaoComponent);
