ed52a82bf590298ed18d2771085859592d369e10
Rony Nogueira <ronynogueiras.dev@gmail.com>
2017-11-18 20:54:19 -0200

    adição de service para projeto de extensão resolve #417

---
+++ back/portal-da-transparencia/src/main/java/fabrica/controller/service/extensao/ServiceProjetoExtensao.java
@@ -0,0 +1,45 @@
+package fabrica.controller.service.extensao;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import fabrica.controller.dto.extensao.ProjetoExtensaoDTO;
+import fabrica.model.dao.GenericDAO.BlocoAtualizar;
+import fabrica.model.dao.extensao.ProjetoExtensaoDAO;
+import fabrica.model.dao.TransacaoHibernate;
+import fabrica.model.domain.extensao.ProjetoExtensao;
+
+/**
+ *
+ * @author Rony Nogueira
+ * @version 1.0.0
+ *
+ * Classe responsável por manipular a entidade {@link ProjetoExtensao}
+ */
+public class ServiceProjetoExtensao {
+
+	private final ProjetoExtensaoDAO projetoDAO = new ProjetoExtensaoDAO(new TransacaoHibernate(),
+			ProjetoExtensao.class);
+	/**
+	 * Cria um novo projeto
+	 * @param  final ProjetoExtensaoDTO projetoDTO
+	 * @return       ProjetoExtensaoDTO
+	 */
+	public ProjetoExtensaoDTO criarProjeto(final ProjetoExtensaoDTO projetoDTO) {
+		ProjetoExtensao projeto = new ProjetoExtensao().novo(projetoDTO.getNome(),
+				projetoDTO.getDescricao(), projetoDTO.getInicio(), projetoDTO.getFim(),
+				projetoDTO.getTipo());
+		projetoDAO.criar(projeto);
+		return projetoDTO;
+	}
+  /**
+   * Busca todos os Projetos de Extensão
+   * @return Collection<ProjetoExtensaoDTO>
+   */
+	public Collection<ProjetoExtensaoDTO> buscarTodas() {
+		return projetoDAO.buscarTodos()
+				        .stream()
+				        .map(ProjetoExtensaoDTO::converterDominio)
+				        .collect(Collectors.toList());
+	}
+}
