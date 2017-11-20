01e2cdbe03050fa37040043b54aabbad49f684cc
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-11-19 23:19:15 -0200

    Criada screen que conterá o projeto único

---
+++ front/src/app/pesquisa/screens/projeto/projeto.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'inf-projeto',
+  templateUrl: './projeto.component.html',
+  styleUrls: ['./projeto.component.scss']
+})
+export class ProjetoScreenComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
