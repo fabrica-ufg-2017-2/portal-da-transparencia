9e7627a109be0a6b5891176810f95ca084e8b8b5
Erivan <erivan.nas@gmail.com>
2017-11-23 17:50:06 -0200

    Atualiza classe Portaria DAO
    Adicionado método para buscar portarias por numero.

--- back/portal-da-transparencia/src/main/java/fabrica/model/dao/PortariaDAO.java
+++ back/portal-da-transparencia/src/main/java/fabrica/model/dao/PortariaDAO.java
@@ -1,21 +1,24 @@
 package fabrica.model.dao;
 
+import javax.persistence.NoResultException;
 import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
-import org.hibernate.Criteria;
 
 import fabrica.model.domain.Portaria;
 
+/**
+ * @author Erivan
+ *
+ */
 public class PortariaDAO extends GenericDAO<Portaria, Long> {
 
 	public PortariaDAO(ProvedorTransacao transacao, Class<Portaria> clazzP) {
 		super(transacao, clazzP);
 	}
 
+	/**
+	 * Método buscaPorNumero busca um portaria através do número.
+	 */
 	public Portaria buscaPorNumero(Long numero) {
 
 		return (Portaria) trs.txr((em) -> {
@@ -23,10 +26,14 @@ public class PortariaDAO extends GenericDAO<Portaria, Long> {
 			Query query = em.createQuery("SELECT p FROM Portaria p where p.numero = :num");
 			query.setParameter("num", numero);
 
-			Portaria portaria = (Portaria) query.getSingleResult();
-
+			Portaria portaria = null;
+			try {
+			portaria =  (Portaria) query.getSingleResult();
+			}
+			catch (NoResultException e) {
+				portaria = new Portaria();
+			}
 			return portaria;
 		});
 	}
-
 }
