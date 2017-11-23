acf26d8c374bea4ce1343ea747990289b6f86b1d
TiagoDamascena <tiagobdamascena@gmail.com>
2017-11-23 21:31:33 -0200

    Tela gestão removida

--- front/src/app/gestao/gestao.module.ts
+++ front/src/app/gestao/gestao.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { PortariasContainerComponent } from './containers/portarias.container';
 
 import { PortariasScreenComponent } from './screens/portarias/portarias.component';
-import { GestaoComponent } from './screens/gestao/gestao.component';
 import { GestaoRoutingModule } from './gestao-routing.module';
 import { GestaoService } from './services/gestao.service';
 
@@ -17,13 +16,12 @@ import {PortariasService} from './services/portarias.service';
   declarations: [
     PortariasScreenComponent,
     PortariasContainerComponent,
-    GestaoComponent,
     LeftSideBarComponent,
     PortariasComponent,
     PortariaComponent
   ],
   imports: [CommonModule, GestaoRoutingModule],
-  exports: [GestaoComponent],
+  exports: [],
   providers: [
     GestaoService,
     PortariasService
