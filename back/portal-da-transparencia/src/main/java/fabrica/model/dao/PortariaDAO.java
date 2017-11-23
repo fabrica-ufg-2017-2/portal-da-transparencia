59d4f7816ee96bddecb9ddeed9b92940c6be1441
Erivan <erivan.nas@gmail.com>
2017-11-23 15:01:11 -0200

    Implementa a busca de portarias por numero

--- back/portal-da-transparencia/src/main/java/fabrica/model/dao/PortariaDAO.java
+++ back/portal-da-transparencia/src/main/java/fabrica/model/dao/PortariaDAO.java
@@ -1,11 +1,32 @@
 package fabrica.model.dao;
 
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Criteria;
+
 import fabrica.model.domain.Portaria;
 
-public class PortariaDAO extends GenericDAO<Portaria, Long>{
+public class PortariaDAO extends GenericDAO<Portaria, Long> {
 
 	public PortariaDAO(ProvedorTransacao transacao, Class<Portaria> clazzP) {
 		super(transacao, clazzP);
+	}
+
+	public Portaria buscaPorNumero(Long numero) {
+
+		return (Portaria) trs.txr((em) -> {
+
+			Query query = em.createQuery("SELECT p FROM Portaria p where p.numero = :num");
+			query.setParameter("num", numero);
+
+			Portaria portaria = (Portaria) query.getSingleResult();
+
+			return portaria;
+		});
 	}
 
 }
